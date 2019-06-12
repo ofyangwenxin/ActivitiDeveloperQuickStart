0dac69a297cf202f71da0ee2b1764f5607b389e6
yangwenxin <yangwenxin45@gmail.com>
2019-06-12 18:18:17 +0800

    ActivitiDeveloperQuickStart

---
+++ src/main/java/com/example/AutomatedDataDelegate.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+import java.util.Date;
+
+public class AutomatedDataDelegate implements JavaDelegate {
+
+    public void execute(DelegateExecution execution) {
+        Date now = new Date();
+        execution.setVariable("autoWelcomeTime", now);
+        System.out.println("Faux call to backend for ["
+                + execution.getVariable("fullName") + "]");
+    }
+}
